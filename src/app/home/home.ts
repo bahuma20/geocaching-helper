0926ae22b067d9f7f5e190c79ce0f61b04fc6884
Max Bachhuber <bahuma20@bahuma.io>
2016-03-03 13:57:19 +0100

    Added load-data-from-file Component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,13 @@
 import {Component} from 'angular2/core';
 import {GeocacheListComponent} from '../geocacheList/geocache-list';
+import {LoadDataFromFileComponent} from '../loadDataFromFile/load-data-from-file';
 
 @Component({
   selector: 'home',
   pipes: [ ],
   styles: [ require('./home.css') ],
   template: require('./home.html'),
-  directives: [GeocacheListComponent]
+  directives: [GeocacheListComponent, LoadDataFromFileComponent]
 })
 export class Home {
   constructor() {
---
+++ src/app/loadDataFromFile/load-data-from-file.ts
@@ -0,0 +1,45 @@
+import {Component} from 'angular2/core';
+import {GeocacheService} from '../geocacheList/services/geocache-service';
+
+@Component({
+  selector: 'load-data-from-file',
+  template: require('./load-data-from-file.html'),
+  styles: [require('./load-data-from-file.css')]
+})
+export class LoadDataFromFileComponent {
+  parsedJSON: Object[];
+  fileReader: FileReader= new FileReader();
+  errors: String[] = [];
+
+  constructor(public geocacheService: GeocacheService) {
+    let self: LoadDataFromFileComponent = this;
+
+    this.fileReader.onload = function(e: ProgressEvent) {
+      let result = e.target['result'];
+      try {
+        self.parsedJSON = JSON.parse(result);
+      } catch (error) {
+        self.errors.push(error.name + ': ' + error.message);
+      }
+    };
+  }
+
+  onFileChanged(event) {
+    // Clear Errors
+    this.errors = [];
+
+    // Get the File
+    let file: File = event.target.files[0];
+
+    // Validate File
+    if (this.validateFile(file)) {
+      this.fileReader.readAsText(file);
+    } else {
+      this.errors.push('Dateiformat nicht unterstuetzt');
+    }
+  }
+
+  validateFile(file: File): boolean {
+    return true;
+  }
+}
